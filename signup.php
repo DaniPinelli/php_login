e6b04fe83ef5dfe070fbfd102371704aedc19d95
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-10-26 16:37:27 -0300

    Adding users to database

--- signup.php
+++ signup.php
@@ -1,13 +1,37 @@
 
 <?php
 require("partials/header.php");
+
+$message = "";
+
+ if(!empty($_POST['email']) && !empty(_POST['password'])) {
+
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+
+    if($stmt->execute()) {
+        $message = "User created successfully";
+    } else {
+        $message = "Something went wrong. Please try again later.";
+    }
+
+ }
 ?>
 
+<?php
 
+ if(!empty($message)) {
+    echo $message;
+ }
+
+?>
 
  <h1>Sign Up</h1>
 
- or <a href="login.php">Login</a>   
+ or <a href="signup.php">Login</a>   
 
 
 
