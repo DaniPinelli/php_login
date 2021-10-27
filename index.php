5add0d6d82f9e3a0124e06ac3ba510e1e557c1d9
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-10-27 10:31:14 -0300

    App finished

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
 require("partials/header.php");
+
+session_start();
+
+if(isset1($_SESSION['user_id'])){
+    $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
+    $records->bindParam(':id', $_SESSION['user_id']);
+    records->execute();
+
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $user = NULL;
+
+    if(count($results) > 0){
+        $user = $results;
+    }
+
+    header("Location: index.php");
+}
 ?>
 
 
-
-
-
+<?php if(!empy(user)): ?>
+<br>Welcome. <?= $user['email']; ?>
+<br>You are logged in
+<a href="logout.php">Logout</a>
+<?php else: ?>
 
 <h1>Login or Sign Up</h1>
 
--- login.php
+++ login.php
@@ -1,26 +1,42 @@
 
 <?php
+
+session_start();
+
+if(isset($_SESSION['user_id'])) {
+    header('Location: /php-login');
+}
+
 require("partials/header.php");
 
-if (!empty($_POST['email'] && !empty($_POST['password']) )){
+if (!empty($_POST['email']) && !empty($_POST['password'])){
     $records = $conn->prepare('SELECT * FROM users WHERE email = :email');
     $records->bindParam(':email', $_POST['email']);
-    
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $user = $db->getUserByEmail($email);
-    if ($user && password_verify($password, $user['password'])){
-        $_SESSION['user_id'] = $user['id'];
-        header("Location: index.php");
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $message = '';
+
+    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])){
+        $_SESSION['user_id'] = $results['id'];
+        header("Location: /php-login");
     } else {
-        $error = "Invalid email or password";
+        $message = 'Sorry, those credentials do not match';
     }
+
+    
 }
-}
+
 
 ?>
 
 <h1>Login</h1>
+
+<span>or <a href="signup.php">SignUp</a></span>
+
+<?php if(!empty($message)): ?>
+    <p><?= $message; ?></p>
+<?php endif; ?>
 
 <form action="login.php" method="post">
     <fieldset>
